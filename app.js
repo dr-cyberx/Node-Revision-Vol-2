0b6ddac536f1d236754927fc8be6585c1fe349de
ItsMeVishal007 <vishal.codedrill@gmail.com>
2022-01-03 18:23:59 +0530

    initializating Project

---
+++ app.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose.connect(process.env.MONGO_URL).then(() => {
+  app.listen(4000, () => {
+    console.log(`The server is up at http://localhost:4000`);
+  });
+});
